4f3422256d15f613d49984b41460561dbcf214ac
HARSH <harshsennn@gmail.com>
2025-08-14 10:29:58 +0530

    api-setup:container

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-const API_BASE = "http://localhost:8081"; // your backend URL
+const API_BASE = "http://localhost:8081";
 
 export const fetchTemplates = () => {
-  return axios.get(`${API_BASE}/templates`).then(res => res.data);
+  return axios.get(`${API_BASE}/templates`).then((res) => res.data);
 };
 
 export const generateCertificate = (data) => {
   return axios.post(`${API_BASE}/generate-certificate`, data, {
-    responseType: "blob" // for downloading PDF
+    responseType: "blob", // for downloading PDF
   });
 };
