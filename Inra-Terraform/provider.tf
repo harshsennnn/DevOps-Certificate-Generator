795b555b28d67e80ebb949640dd23565a369d954
HARSH <harshsennn@gmail.com>
2025-08-14 22:54:36 +0530

    tf files

---
+++ Inra-Terraform/provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  backend "s3" {
+    bucket         = "my-terraform-state-bucket_for_devops_url_2_qr"
+    key            = "global/mystatefile/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-state-lock"
+    encrypt        = true
+
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-south-1"
+}
